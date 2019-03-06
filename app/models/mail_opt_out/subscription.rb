e5e1842a0e2f3bf550a6a0f8f37f41ed8ba9d673
Joel AZEMAR <joel.azemar@gmail.com>
2019-03-06 14:53:34 +0100

    User should not be considered as optional for a Subscription

--- app/models/mail_opt_out/subscription.rb
+++ app/models/mail_opt_out/subscription.rb
@@ -1,6 +1,6 @@
 module MailOptOut
   class Subscription < ApplicationRecord
-    belongs_to :user, polymorphic: true, optional: true
+    belongs_to :user, polymorphic: true
     belongs_to :list, class_name: 'MailOptOut::List'
   end
 end
