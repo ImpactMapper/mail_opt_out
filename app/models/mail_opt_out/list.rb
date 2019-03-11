e00d11348947864d7186fde334ea58deb04e06bf
Joel AZEMAR <joel.azemar@gmail.com>
2019-03-11 17:39:51 +0100

    Add List Name Uniqueness Validation

--- app/models/mail_opt_out/list.rb
+++ app/models/mail_opt_out/list.rb
@@ -1,6 +1,7 @@
 module MailOptOut
   class List < ApplicationRecord
     validates :name, presence: true
+    validates_uniqueness_of :name, case_sensitive: false
     scope :active, -> { where(published: true) }
   end
 end
--- spec/models/mail_opt_out/list_spec.rb
+++ spec/models/mail_opt_out/list_spec.rb
@@ -2,13 +2,33 @@ require 'rails_helper'
 
 module MailOptOut
   RSpec.describe List, type: :model do
-    let(:list) { Fabricate.build(:list) }
+    let(:valid_attributes) { Fabricate.attributes_for(:list) }
+    let(:list)             { List.new(attributes) }
 
-    it do
-      expect(list).to be_valid
-      expect {
-        list.save!
-      }.to_not raise_error
+    context 'valid attributes' do
+      let(:attributes) { valid_attributes }
+
+      it do
+        expect(list).to be_valid
+        expect {
+          list.save!
+        }.to_not raise_error
+      end
+    end
+
+    context 'with an list' do
+      let(:attributes) { valid_attributes }
+
+      before { list.save! }
+
+      context 'attempt create a duplicate' do
+        let(:duplicate) { List.new(valid_attributes.merge(name: valid_attributes[:name].upcase)) }
+
+        it do
+          expect(duplicate).to_not be_valid
+          expect(duplicate.errors.messages).to eql({ name: ['has already been taken'] })
+        end
+      end
     end
   end
 end
